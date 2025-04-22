efa19cce068a6bde561d8f6e835e68575d18a3ab
jrgf <jg1238610@gmail.com>
2025-04-22 09:50:44 -0600

    first commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from mcp.server.fastmcp import FastMCP
+import requests
+
+
+mcp = FastMCP("Pypi MCP",dependencies=["requests"])
+
+
+@mcp.tool()
+def retrieve_package_info(name: str):
+    """
+    Retrieve package information from PyPI"
+    """
+    try:
+        response = requests.get(f"https://pypi.org/pypi/{name}/json")
+        if response.status_code == 200:
+            return {
+                "latest_version": response.json()["info"]["version"],
+                "name": response.json()["info"]["name"],
+                "requirements": response.json()["info"]["requires_dist"],
+                "license": response.json()["info"]["license"],
+                "summary": response.json()["info"]["summary"],  
+            }
+        else:
+            print(f"Package {name} not found.")
+            return None
+    except Exception as e:
+        print(f"Error retrieving package info: {e}")
+        return None
